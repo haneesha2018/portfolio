3a80f3ff0f27636250eadbd0c1aedc4e8f0bc423
Haneesha Dushara <haneesha207@gmail.com>
2024-10-28 17:01:19 -0400

    Refactor imports and update text in Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,12 +23,12 @@ const Hero = () => {
                 <div className="flex justify-center relative my-20 z-10">
                 <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
                 <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'> 
-                    Hello, I'm Haneesha Dushara
+                    Hello, I&apos;m Haneesha Dushara
                 </h2>
 
                 <TextGenerateEffect className='text-center text-[40px] md:text-5xl lg:text-6xl' words='Aspiring Software Engineer'/>
             <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I'm Haneesha.
+            Hi! I&apos;m Haneesha.
             </p>
             <a href="/resume/Haneesha_Dushara_Full_Time_Resume.pdf" target="_blank" rel="noopener noreferrer">
             <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border borde</a>r-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
