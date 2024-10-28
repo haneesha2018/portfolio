7f7ee2e732afe707b84dd00910c6b161dcfc010f
Haneesha Dushara <haneesha207@gmail.com>
2024-10-28 04:55:23 -0400

    Refactor FloatingNav component and add navigation items

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import Hero from "../components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col over mx-auto sm:px-10 px-5">   
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={
+          [
+            { name: "Home", link: "/" },
+            { name: "Projects", link: "/projects" },
+            { name: "Contact", link: "/contact" },
+          ]
+        } />
         < Hero />
       </div>
     </main>
